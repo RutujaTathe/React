874d14292373da27e1dfbe286422781f9da1ae8f
RutujaTathe <rutujatathe.2211@gmail.com>
2025-05-08 00:06:41 +0530

    igniting our aap

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-React ✨🎉
+# React ✨🎉
+
+# Parcel
+
+- dev build
+- local server
+- HMR(hot module replacement/auto refresh)
+- file watching algorithm -written in c++
+- Caching-faster build
+- image optimization
+- Minification of file
+- bundling
+- compressing
+- consistent hashing
+- code splitting
+- deferential bundling - support older browser
+- Dignostic
+- Error Handling
+- HTTPs
+- Tree Shaking -remove unused code
+- Different dev and prod bundles
--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 // const heading = React.createElement(
 //   "h1",
 //   { id: "heading" },
@@ -10,12 +13,12 @@
 
 const element = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "Child one h1 tag"),
-    React.createElement("h2", {}, "child 1 h2 tag"),
+    React.createElement("h1", { key: "item1" }, "Child one  tag"),
+    React.createElement("h2", { key: "item2" }, "child 1 two tag"),
   ]),
   React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "child 2 h1 tag"),
-    React.createElement("h2", {}, "child 2 h2 tag"),
+    React.createElement("h1", { key: "item3" }, "child 2 h1 tag"),
+    React.createElement("h2", { key: "item4" }, "child 2 h2 tag"),
   ]),
 ]);
 console.log("element", element);
